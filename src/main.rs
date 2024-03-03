0ea9606bbe57ba8d744a9d7c5addc5b118b28756
Jan Popelka <popojan@protonmail.com>
2024-03-03 12:36:05 +0100

    fix: raw output always sorted

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,7 @@ fn as_egyptian_fraction_raw(x0: &BigInt, y0: &BigInt, _reverse: bool) -> Vec<(Bi
         ret.extend(vec![(x.clone(), y.clone())]);
     }
     ret.extend(whole);
-    ret.sort();
+    ret.sort_by(|x, y| { x.1.cmp(&y.1)});
     ret
 }
 
